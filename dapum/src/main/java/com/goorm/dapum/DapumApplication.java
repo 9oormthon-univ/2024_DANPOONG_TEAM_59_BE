eee84931023cee3a0de280e673302100fe361aeb
gyugyugay <werty1234179@naver.com>
2024-11-23 04:10:13 +0900

    feat : 기본 환경 세팅

---
+++ dapum/src/main/java/com/goorm/dapum/DapumApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.dapum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapumApplication.class, args);
+	}
+
+}
