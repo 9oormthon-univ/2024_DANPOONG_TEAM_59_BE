273ebd1240d68d3e89cd8f7358152b38fb20bbd4
gyugyugay <werty1234179@naver.com>
2024-11-23 04:35:30 +0900

    feat : JwtService import문 추가

--- dapum/src/main/java/com/goorm/dapum/component/AuthorizationInterceptor.java
+++ dapum/src/main/java/com/goorm/dapum/component/AuthorizationInterceptor.java
@@ -1,5 +1,6 @@
 package com.goorm.dapum.component;
 
+import com.goorm.dapum.service.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
