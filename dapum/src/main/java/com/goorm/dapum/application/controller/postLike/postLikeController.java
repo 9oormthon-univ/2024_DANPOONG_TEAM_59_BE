6012ac53ab0a5af523bc8d5c706c0a910bcf122f
gyugyugay <werty1234179@naver.com>
2024-11-24 03:32:03 +0900

    chore : api 설명 추가

--- dapum/src/main/java/com/goorm/dapum/application/controller/postLike/postLikeController.java
+++ dapum/src/main/java/com/goorm/dapum/application/controller/postLike/postLikeController.java
@@ -2,6 +2,7 @@ package com.goorm.dapum.application.controller.postLike;
 
 import com.goorm.dapum.domain.postLike.dto.PostLikeResponse;
 import com.goorm.dapum.domain.postLike.service.PostLikeService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ public class postLikeController {
     private final PostLikeService postLikeService;
 
     @PostMapping("/{postId}/likes/toggle")
+    @Operation(summary = "좋아요/좋아요 취소")
     public ResponseEntity<PostLikeResponse> toggleLike(@PathVariable Long postId) {
         postLikeService.toggleLike(postId);
         long likeCount = postLikeService.getLikeCount(postId);
