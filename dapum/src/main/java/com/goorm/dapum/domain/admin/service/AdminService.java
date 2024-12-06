e386411a63cf48411e6e8b8c39c40cbb59665651
gyugyugay <werty1234179@naver.com>
2024-12-07 04:12:17 +0900

    feat : 신고 처리를 위한 logic 구현 완료

--- dapum/src/main/java/com/goorm/dapum/domain/admin/service/AdminService.java
+++ dapum/src/main/java/com/goorm/dapum/domain/admin/service/AdminService.java
@@ -49,4 +49,39 @@ public class AdminService {
                 .collect(Collectors.toList());
     }
 
+    // 게시글 신고 처리하기
+    public void processPostReports(PostReportRequest request) throws BadRequestException {
+        // 신고된 게시글을 찾는다.
+        PostReport report = postReportRepository.findById(request.postReportId())
+                .orElseThrow(() -> new BadRequestException("해당 신고를 찾을 수 없습니다."));
+
+        // 신고 상태가 '신고처리'일 경우 게시글을 삭제하고 신고를 처리
+        if (request.state().equals("신고처리")) {
+            // 게시글 삭제
+            postService.DeletePost(report.getPost().getId());
+        }
+        // 신고 상태가 '신고반려'일 경우 해당 게시글을 신고한 모든 신고를 삭제
+        else if (request.state().equals("신고반려")) {
+            // 해당 게시글을 신고한 모든 신고 삭제
+            postReportRepository.deleteByPostId(report.getPost().getId());
+        } else {
+            throw new BadRequestException("잘못된 신고 상태입니다.");
+        }
+    }
+
+    // 돌봄 게시글 신고 처리하기
+    public void processCarePostReports(CareReportRequest request) throws BadRequestException {
+        CareReport report = careReportRepository.findById(request.careReportId())
+                .orElseThrow(() -> new BadRequestException("해당 신고를 찾을 수 없습니다."));
+
+        if(request.state().equals("신고처리")){
+            carePostService.deleteCarePost(report.getCarePost().getId());
+        }
+        else if (request.state().equals("신고반려")) {
+            // 해당 게시글을 신고한 모든 신고 삭제
+            careReportRepository.deleteByPostId(report.getCarePost().getId());
+        } else {
+            throw new BadRequestException("잘못된 신고 상태입니다.");
+        }
+    }
 }
