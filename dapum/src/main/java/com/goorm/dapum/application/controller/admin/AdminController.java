61b6c19a336443472dc5bd1b771a8ef9e6b3193f
gyugyugay <werty1234179@naver.com>
2024-12-07 04:12:30 +0900

    feat : 관리자의 신고를 위한 api 연결 완료

---
+++ dapum/src/main/java/com/goorm/dapum/application/controller/admin/AdminController.java
@@ -0,0 +1,47 @@
+package com.goorm.dapum.application.controller.admin;
+
+import com.goorm.dapum.domain.admin.dto.CareReportRequest;
+import com.goorm.dapum.domain.admin.dto.PostReportRequest;
+import com.goorm.dapum.domain.admin.service.AdminService;
+import com.goorm.dapum.domain.carePost.dto.CarePostListResponse;
+import com.goorm.dapum.domain.post.dto.PostListResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    @Autowired
+    private final AdminService adminService;
+
+    @GetMapping("/posts")
+    @Operation(summary = "게시글 신고 목록 가져오기")
+    public List<PostListResponse> getPostReportList() {
+        return adminService.getPostReportList();
+    }
+
+    @GetMapping("/carePosts")
+    @Operation(summary = "돌봄 게시글 신고 목록 가져오기")
+    public List<CarePostListResponse> getCarePostReportList() {
+        return adminService.getCarePostReportList();
+    }
+
+    @PutMapping("/posts")
+    @Operation(summary = "게시글 신고처리 하기")
+    public void processPostReports(@RequestBody PostReportRequest request) throws BadRequestException {
+        adminService.processPostReports(request);
+    }
+
+    @PutMapping("/carePosts")
+    @Operation(summary = "돌봄 신고처리 하기")
+    public void processCarePostReports(@RequestBody CareReportRequest request) throws BadRequestException {
+        adminService.processCarePostReports(request);
+    }
+}
