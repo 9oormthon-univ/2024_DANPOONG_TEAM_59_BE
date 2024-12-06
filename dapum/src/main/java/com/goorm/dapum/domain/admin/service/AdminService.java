04eacd3f42201d7d0185ca2cdc225efbbd190d06
gyugyugay <werty1234179@naver.com>
2024-12-07 04:07:39 +0900

    feat : 게시글 불러오기 코드 간소화

--- dapum/src/main/java/com/goorm/dapum/domain/admin/service/AdminService.java
+++ dapum/src/main/java/com/goorm/dapum/domain/admin/service/AdminService.java
@@ -2,6 +2,8 @@ package com.goorm.dapum.domain.admin.service;
 
 import com.goorm.dapum.domain.PostReport.entity.PostReport;
 import com.goorm.dapum.domain.PostReport.repository.PostReportRepository;
+import com.goorm.dapum.domain.admin.dto.CareReportRequest;
+import com.goorm.dapum.domain.admin.dto.PostReportRequest;
 import com.goorm.dapum.domain.carePost.dto.CarePostListResponse;
 import com.goorm.dapum.domain.carePost.service.CarePostService;
 import com.goorm.dapum.domain.carePostReport.entity.CareReport;
@@ -10,11 +12,12 @@ import com.goorm.dapum.domain.post.dto.PostListResponse;
 import com.goorm.dapum.domain.post.service.PostService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -34,26 +37,16 @@ public class AdminService {
 
     // 게시글 신고 목록 가져오기
     public List<PostListResponse> getPostReportList() {
-        List<PostReport> reports = postReportRepository.findAll();
-        List<PostListResponse> responses = new ArrayList<>();
-
-        for (PostReport report : reports) {
-            PostListResponse response = postService.createPostListResponse(report.getPost());
-            responses.add(response);
-        }
-        return responses;
+        return postReportRepository.findAll().stream()
+                .map(report -> postService.createPostListResponse(report.getPost()))
+                .collect(Collectors.toList());
     }
 
     // 돌봄 게시글 신고 목록 가져오기
     public List<CarePostListResponse> getCarePostReportList() {
-        List<CareReport> reports =  careReportRepository.findAll();
-        List<CarePostListResponse> responses = new ArrayList<>();
-
-        for (CareReport report : reports) {
-            CarePostListResponse response = carePostService.createCarePostListResponse(report.getCarePost());
-            responses.add(response);
-        }
-
-        return responses;
+        return careReportRepository.findAll().stream()
+                .map(report -> carePostService.createCarePostListResponse(report.getCarePost()))
+                .collect(Collectors.toList());
     }
+
 }
