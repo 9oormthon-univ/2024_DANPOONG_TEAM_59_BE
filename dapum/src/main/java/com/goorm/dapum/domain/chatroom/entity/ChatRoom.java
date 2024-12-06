6e3d94862cb90bfca2662e9f31e1de774e2a384e
gyugyugay <werty1234179@naver.com>
2024-12-06 15:23:53 +0900

    feat : 채팅방 entity 수정 완료 (게시글과 돌봄게시글 둘다 채팅방 만들 수 있도록)

--- dapum/src/main/java/com/goorm/dapum/domain/chatroom/entity/ChatRoom.java
+++ dapum/src/main/java/com/goorm/dapum/domain/chatroom/entity/ChatRoom.java
@@ -4,6 +4,7 @@ import com.goorm.dapum.core.base.BaseEntity;
 import com.goorm.dapum.domain.member.entity.Member;
 import com.goorm.dapum.domain.message.entity.Message;
 import com.goorm.dapum.domain.carePost.entity.CarePost;
+import com.goorm.dapum.domain.post.entity.Post;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,11 +29,59 @@ public class ChatRoom extends BaseEntity {
     @JoinColumn(name = "member2_id", nullable = false)
     private Member member2;  // 참여자 2
 
+    @Enumerated(EnumType.STRING) // Enum 타입 매핑
+    @Column(nullable = false)
+    private ChatRoomTag chatRoomTag; // 채팅방 태그 ("돌봄" 또는 "나눔")
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "care_post_id", nullable = true)
     private CarePost carePost;  // 해당 채팅방이 속한 CarePost
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = true)
+    private Post post;  // 해당 채팅방이 속한Post
+
     @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Message> messages = new ArrayList<>();  // 해당 채팅방의 메시지들
 
+    @Column(nullable = false)
+    private boolean tradeCompleted = false; // 거래 완료 여부
+
+    @Column(nullable = false)
+    private boolean reviewCompleted = false; // 후기 작성 여부
+
+    // 생성자
+    public ChatRoom(Member member1, Member member2, ChatRoomTag chatRoomTag) {
+        this.member1 = member1;
+        this.member2 = member2;
+        this.chatRoomTag = chatRoomTag;
+    }
+
+    public ChatRoom() {
+
+    }
+
+    // CarePost 기반 채팅방 생성
+    public static ChatRoom createCareChatRoom(Member member1, Member member2, CarePost carePost) {
+        ChatRoom chatRoom = new ChatRoom(member1, member2, ChatRoomTag.CARE);
+        chatRoom.setCarePost(carePost);
+        return chatRoom;
+    }
+
+    // Post 기반 채팅방 생성
+    public static ChatRoom createPostChatRoom(Member member1, Member member2, Post post) {
+        ChatRoom chatRoom = new ChatRoom(member1, member2, ChatRoomTag.SHARE);
+        chatRoom.setPost(post);
+        return chatRoom;
+    }
+
+    // 거래 완료 설정
+    public void completeTrade() {
+        this.tradeCompleted = true;
+    }
+
+    // 후기 작성 완료 설정
+    public void completeReview() {
+        this.reviewCompleted = true;
+    }
 }
